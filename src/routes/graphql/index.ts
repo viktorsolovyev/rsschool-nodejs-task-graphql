dd17bf60ae5857c4cba4e79bdb8992b0f05259dc
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-23 00:00:56 +0300

    feat: add memberTypes queries implementation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,11 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLSchema } from 'graphql';
+import { rootQuery } from './rootQuery.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +16,19 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+
+      const schema = new GraphQLSchema({
+        query: rootQuery,        
+      });
+
+      const res = await graphql({
+        schema: schema,
+        source: query,
+        variableValues: variables,
+        contextValue: { prisma },
+      });
+      return res;
     },
   });
 };
---
+++ src/routes/graphql/rootQuery.ts
@@ -0,0 +1,29 @@
+import { GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { memberTypeType, memberTypesType, MemberTypeId } from './types/memberTypes.js';
+import { Context } from './types/context.type.js';
+
+export const rootQuery = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    memberType: {
+      type: memberTypeType,
+      args: {
+        id: { type: new GraphQLNonNull(MemberTypeId) },
+      },
+
+      async resolve(parent, args: { id: string }, ctx: Context) {
+        console.log(args.id);
+        return await ctx.prisma.memberType.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+
+    memberTypes: {
+      type: memberTypesType,
+      async resolve(parent, args, ctx: Context) {
+        return await ctx.prisma.memberType.findMany();
+      },
+    },
+  },
+});
