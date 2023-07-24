baa2246fd4e198c893f2383b79682de951244286
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-24 22:12:56 +0300

    feat: add changePost mutation

--- src/routes/graphql/rootMutation.ts
+++ src/routes/graphql/rootMutation.ts
@@ -3,7 +3,7 @@ import { UserType } from './users/users.js';
 import { Context } from './types/context.type.js';
 import { createUserInput } from './users/inputs.js';
 import { PostType } from './posts/posts.js';
-import { createPostInput } from './posts/inputs.js';
+import { createPostInput, changePostInput } from './posts/inputs.js';
 import { UUID } from 'crypto';
 import { ProfileType } from './profiles/profiles.js';
 import { createProfileInput } from './profiles/inputs.js';
@@ -52,6 +52,18 @@ export const rootMutation = new GraphQLObjectType({
         ctx: Context,
       ) {
         return await ctx.prisma.post.create({ data: args.dto });
+      },
+    },
+
+    changePost: {
+      type: PostType,
+      args: { id: { type: UUIDType }, dto: { type: changePostInput } },
+      async resolve(
+        root,
+        args: { id: UUID; dto: { authorId: UUID; content: string; title: string } },
+        ctx: Context,
+      ) {
+        await ctx.prisma.post.update({ where: { id: args.id }, data: args.dto });
       },
     },
 
