d6fd97526803e38bbebac93a9f66aa3109875f79
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-25 00:49:09 +0300

    feat: add graphql-depth-limit implementation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLSchema } from 'graphql';
+import { graphql, GraphQLError, GraphQLSchema, parse, validate } from 'graphql';
 import { rootQuery } from './rootQuery.js';
 import { rootMutation } from './rootMutation.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -23,6 +24,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         query: rootQuery,
         mutation: rootMutation,
       });
+
+      const validationErrors: readonly GraphQLError[] = validate(schema, parse(query), [
+        depthLimit(5),
+      ]);
+      if (validationErrors.length) return { data: '', errors: validationErrors };
 
       const res = await graphql({
         schema: schema,
